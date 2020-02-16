fa8b72b3974bfcf1bf6a516d85b681af278d3eed
Andre-ti-dev <andreluiz.ti.dev@gmail.com>
2020-02-16 13:13:54 -0300

    Ajax com XMLHttpRequest

---
+++ 4 - JS assincrono/main.js
@@ -0,0 +1,9 @@
+let xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'https://api.github.com/users/andre-ti-dev');
+xhr.send(null);
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4) {
+    console.log(JSON.parse(xhr.responseText));
+  }
+};
